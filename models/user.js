c814cc0dbe5549da6b82ed9f5c291497faf91a08
eguantay <eguantay@silentiumapps.com>
2020-07-28 00:24:33 -0300

    POST PUT User

--- models/user.js
+++ models/user.js
@@ -1,13 +1,21 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var Schema = mongoose.Schema;
+
+var rolesValidator = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol permitido'
+}
 
 var userSchema = new Schema({
     name: { type: String, required: [true, 'El nombre es necesario']},
     email: { type: String, unique: true, required: [true, 'El email es necesario'] },
     password: { type: String, required: [true, 'La contraseña es necesaria'] },
     img: { type: String, required: false },
-    role: { type: String, required: true, default:'USER_ROLE' }
+    role: { type: String, required: true, default:'USER_ROLE', enum: rolesValidator }
 });
+
+userSchema.plugin(uniqueValidator, {message: '{PATH} debe ser unico'});
 
 module.exports = mongoose.model('users', userSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 // Requires
 var express = require('express');
+var bcrypt = require('bcryptjs');
+
 var app = express();
 
 //Model User
@@ -30,6 +32,57 @@ app.get('/', (request, response, next) => {
 });
 
 //===========================
+//ACTUALIZAR USUARIO PUT
+//============================
+app.put('/:id', (req, resp)=>{
+    
+    var id = req.params.id;
+    var body = req.body;
+    
+    console.log(body)
+    User.findById( id, (err, user) =>{
+        if (err){
+            return resp.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if (!user){
+            console.log("user");
+            return resp.status(400).json({
+                ok: false,
+                mensaje: 'El usuario con el id'+ id+' no existe',
+                errors: { message: 'No existe usuario con ese id'}
+            });
+        }
+
+        user.name = body.name;
+        user.email = body.email;
+        user.role = body.role;
+
+        user.save((err, userSave) =>{
+            if (err){
+                console.log("error", err);
+                return resp.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar usuario',
+                    errors: err
+                });
+            }
+
+            userSave.password = '';
+
+            resp.status(200).json({
+                ok: true,
+                usuarios: userSave
+            });
+        });  
+    })
+})
+
+//===========================
 //Crear usuario POST
 //============================
 app.post('/',(req, resp)=>{
@@ -37,14 +90,14 @@ app.post('/',(req, resp)=>{
     var user = new User({
         name: body.name,
         email: body.email,
-        password: body.password,
+        password: bcrypt.hashSync(body.password, 10),
         img: body.img,
         role: body.role
     });
 
     user.save( (err, userSave) =>{
         if (err){
-            return resp.status(500).json({
+            return resp.status(400).json({
                 ok: false,
                 mensaje: 'Error al crear Users',
                 errors: err
@@ -54,7 +107,7 @@ app.post('/',(req, resp)=>{
         resp.status(201).json({
             ok: true,
             usuarios: userSave
-        })
+        });
     });
 });
 
